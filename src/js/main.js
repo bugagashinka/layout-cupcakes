eb0552467dfc7868177c1c58b137e1006f7a3d0a
bugagashinka <voloshyn.andrew@gmail.com>
2020-03-19 16:44:15 +0200

    Fix header section
    -Add logo
    -Add header slides images
    -Header__top section color change
    -Animated scroll to target section
    -Add header__top section shadow

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,34 @@
 $(function() {
-  $(".header__slider").slick({
-    autoplay: false,
-    slidesToShow: 1,
-    slidesToScroll: 1
+  // *** Global ***
+
+  // Animated scroll to target section
+  $(document).on("click", 'a[href^="#"]', function(event) {
+    event.preventDefault();
+    var $nav = $(".header__top");
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top - $nav.outerHeight()
+      },
+      500
+    );
   });
+
+  // *** End Global ***
+
+  // *** Home page ***
+
+  // Header slider
+  $(".header__slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1200
+  });
+
+  // Header top menu section
+  $(document).scroll(function() {
+    var $nav = $(".header__top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() >= window.innerHeight - $nav.outerHeight() / 2);
+  });
+
+  // *** End Home page ***
 });
